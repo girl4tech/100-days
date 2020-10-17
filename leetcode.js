b43888a4ab585962dab08aecebba5d45f707c3e2
Girl4Tech <danirak21@gmail.com>
2020-10-17 19:50:38 -0400

    day 9

--- leetcode.js
+++ leetcode.js
@@ -182,3 +182,16 @@ public class ParkingSystem {
         return --p[carType - 1] >= 0;
     }
 }
+
+// Given the array candies and the integer extraCandies, where candies[i] represents the number of candies that the ith kid has.
+// For each kid check if there is a way to distribute extraCandies among the kids such that he or she can have the greatest number of 
+// candies among them. Notice that multiple kids can have the greatest number of candies.
+
+public class Solution 
+{
+    public IList<bool> KidsWithCandies(int[] candies, int extraCandies) 
+    {
+        int max = candies.Max();
+        return candies.Select(x => (x + extraCandies) >= max).ToList();
+    }
+}
