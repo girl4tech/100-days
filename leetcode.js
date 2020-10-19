920b660740de708b6f20f931d54f48ada86dd369
Girl4Tech <danirak21@gmail.com>
2020-10-18 22:54:58 -0400

    day 10

--- leetcode.js
+++ leetcode.js
@@ -195,3 +195,24 @@ public class Solution
         return candies.Select(x => (x + extraCandies) >= max).ToList();
     }
 }
+
+// You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  
+// Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+// The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, 
+// so "a" is considered a different type of stone from "A".
+
+public int NumJewelsInStones(string J, string S) {
+    	if(J.Length == 0)
+		{
+			return 0;
+		}
+		int numb = 0;
+		for (int i = 0; i < S.Length; i++)
+		{
+			if(J.Contains(S[i]))
+			{
+				numb++;
+			}
+		}
+		return numb;
+}
