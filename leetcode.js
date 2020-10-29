aaa4ad280c511db695be3f07105950dd96b594d4
Girl4Tech <danirak21@gmail.com>
2020-10-28 20:27:20 -0400

    day 12

--- leetcode.js
+++ leetcode.js
@@ -241,3 +241,10 @@ public int NumIdenticalPairs(int[] nums)
 	}
 	return result;
 }
+
+// Given an array, rotate the array to the right by k steps, where k is non-negative.
+var rotate = function(nums, k) {
+    for (let i = 0; i < k; i++) {
+        nums.unshift(nums.pop());
+    }
+};
