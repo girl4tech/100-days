fadc0b0f45b6c9094865e3f8ca2c28774a77ebc7
Girl4Tech <danirak21@gmail.com>
2020-10-29 22:08:19 -0400

    day 13

--- leetcode.js
+++ leetcode.js
@@ -248,3 +248,16 @@ var rotate = function(nums, k) {
         nums.unshift(nums.pop());
     }
 };
+
+// Given the array candies and the integer extraCandies, where candies[i] represents the number of candies that the ith kid has.
+// For each kid check if there is a way to distribute extraCandies among the kids such that
+// he or she can have the greatest number of candies among them. Notice that multiple kids can have the greatest number of candies.
+
+var kidsWithCandies = function(candies, extraCandies) {
+    var max = Math.max(...candies);
+    var result = [];
+    for(var i =0;i<candies.length;i++){
+        result.push(candies[i] + extraCandies >= max);
+    }
+    return result;
+};
