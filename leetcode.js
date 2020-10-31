c5dc336f63d687e9d0e202ac3ca8bcb666390fb9
Girl4Tech <danirak21@gmail.com>
2020-10-30 21:13:52 -0400

    day 14

--- leetcode.js
+++ leetcode.js
@@ -261,3 +261,17 @@ var kidsWithCandies = function(candies, extraCandies) {
     }
     return result;
 };
+
+// Given an array of integers nums.
+// A pair (i,j) is called good if nums[i] == nums[j] and i < j.
+// Return the number of good pairs.
+
+var numIdenticalPairs = function(nums) {
+    let count = 0;
+    for(let i=0; i<nums.length; i++){
+        for(let j=i+1; j<nums.length; j++){
+            if(nums[i] == nums[j]) count++;
+        }
+    }
+    return count;
+};
