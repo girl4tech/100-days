8fe9230ff39505a00843c4b076c0fa98b8aad372
Girl4Tech <danirak21@gmail.com>
2020-11-02 22:47:16 -0500

    day 15

--- leetcode.js
+++ leetcode.js
@@ -291,3 +291,18 @@ var numJewelsInStones = (J, S) => {
     }   
     return count;
 };
+
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
+var multiples3or5 = function(){
+    let sum = 0;
+    for(let i=0; i<1000; i++){
+        if (i%3 === 0 || i%5 === 0){
+            sum+=i;
+        }
+    }
+// answer = 233168
+    return sum;
+}
+console.log(multiples3or5());
