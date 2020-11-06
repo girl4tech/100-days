53caefe2d36b3802e2b23446442c1a1868e1ad4f
Girl4Tech <danirak21@gmail.com>
2020-11-05 22:28:30 -0500

    day 18

--- leetcode.js
+++ leetcode.js
@@ -344,3 +344,19 @@ function evenFibSum(){
 // answer : 4613732
     console.log(sum);
 }
+
+// Given a non-negative integer num, return the number of steps to reduce it to zero. 
+// If the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
+var numberOfSteps  = function(num) {
+    var steps = 0;
+    while (num > 0) {
+        if (num % 2 == 0) {
+            num = num / 2;
+            steps++;
+        } else {
+            num--;
+            steps++;
+        }
+    }
+    return steps;
+};
