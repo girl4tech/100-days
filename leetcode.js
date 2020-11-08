4eab410bf757f47e2b1b3b64947dce7b59b7bcdf
Girl4Tech <danirak21@gmail.com>
2020-11-07 21:17:36 -0500

    day 20

--- leetcode.js
+++ leetcode.js
@@ -371,3 +371,17 @@ var subtractProductAndSum = function(n) {
     }
     return product - sum
 };
+
+// Given a sorted array nums, remove the duplicates in-place such that each element appears only once and returns the new length.
+// Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+var removeDuplicates = function(nums) {
+    if (nums.length == 0) return 0;
+    let i = 0;
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[j] != nums[i]) {
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+    return i + 1;
+};
