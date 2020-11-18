ca8c04253f7cf0fb949eb769cf9e87c1176efe99
Girl4Tech <danirak21@gmail.com>
2020-11-17 22:40:43 -0500

    day 30

--- leetcode.js
+++ leetcode.js
@@ -520,3 +520,19 @@ const moveZeroes = nums => {
   }
   return;
 };
+
+// Given an array A of non-negative integers, return an array consisting of all the even elements of A, followed by all the odd elements of A.
+var sortArrayByParity = function(A) {
+    let even = [];
+    let odd = [];
+   for(let i=0; i<A.length; i++){
+       if(A[i]%2===0){
+           even.push(A[i]);
+       }else{
+           odd.push(A[i]);
+       }
+   }
+   let result = even.concat(odd);
+   return result;
+    
+};
