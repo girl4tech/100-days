b200ce3cee8bfe934244d22ae9d6a37e707ddff4
Girl4Tech <danirak21@gmail.com>
2020-11-22 23:31:54 -0500

    day 35

--- leetcode.js
+++ leetcode.js
@@ -606,3 +606,17 @@ var climbStairs = function(n) {
     let fibn=Math.pow((1+sqrt5)/2,n+1)-Math.pow((1-sqrt5)/2,n+1);
     return (fibn/sqrt5);
 };
+
+// Write a function that reverses a string. The input string is given as an array of characters char[].
+// Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+// You may assume all the characters consist of printable ascii characters.
+var reverseString = function(s) {
+   
+    for(let i = 0, temp; i < s.length/2 ; i++){
+        temp = s[i];
+        s[i] = s[s.length-i-1];
+        s[s.length-i-1] = temp;
+    }
+     return s;
+    
+};
