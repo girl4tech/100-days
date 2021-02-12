ff2c126f74643e4925fb6b49f396a9192a89c4ed
Girl4Tech <danirak21@gmail.com>
2021-02-12 18:17:35 -0500

    Update leetcode.js

--- leetcode.js
+++ leetcode.js
@@ -651,3 +651,14 @@ var swapPairs = function(head) {
   
   return first;
 };
+
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+var maxProfit = function(prices) {
+    let max=0;
+    for(let i=0; i<prices.length; i++){
+        if(prices[i] > prices[i-1]){
+            max += prices[i] - prices[i-1];
+        }
+    }
+    return max;
+};
